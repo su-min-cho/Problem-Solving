3ad0bcce75b8a0fcba19b119bea18217f5243254
조수민 <cho.s.m50@gmail.com>
2026-07-25 17:19:26 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    for(char c : my_string){
+        if(c!=letter[0]) answer.push_back(c);
+    }
+    return answer;
+}
