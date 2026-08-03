84dddfd85ba1f847ee23f33f12d1f7c906a8405b
조수민 <cho.s.m50@gmail.com>
2026-08-03 18:44:45 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(char c : my_string) {
+        if(answer.find(c) == string::npos) {
+            answer += c;
+        }
+    }
+    
+    return answer;
+}
