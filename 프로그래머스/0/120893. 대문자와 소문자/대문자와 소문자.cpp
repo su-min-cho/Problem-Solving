c5d7492a51fb7c964c2033c5557d165cb8ba5910
조수민 <cho.s.m50@gmail.com>
2026-08-05 19:22:29 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -6,7 +6,7 @@ using namespace std;
 string solution(string my_string) {
     string answer = "";
     for(char c : my_string){
-        if(c<=90) answer+=c+32;
+        if(c<='Z') answer+=c+32;
         else answer+=c-32;
     }
     return answer;
