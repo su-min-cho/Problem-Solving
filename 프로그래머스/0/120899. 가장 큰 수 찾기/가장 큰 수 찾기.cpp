03a0413854d53f0183cf9e3b27f538ef0360ad78
조수민 <cho.s.m50@gmail.com>
2026-08-06 17:32:07 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -5,13 +5,8 @@
 using namespace std;
 
 vector<int> solution(vector<int> array) {
-    int max=array[0];
-    int max_idx=0;
-    for(int i = 1; i < array.size(); i++){
-        if(max<array[i]) {
-            max=array[i];
-            max_idx=i;
-        }
-    }
-    return {max, max_idx};
+    auto it = max_element(array.begin(),array.end());
+    int max_val = *it;
+    int max_idx = distance(array.begin(),it);
+    return {max_val, max_idx};
 }
