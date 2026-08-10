650efcf9b5085ad36041f53616906b44637ddc62
조수민 <cho.s.m50@gmail.com>
2026-08-10 16:33:31 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    for(int h : array){
+        if(h>height) answer++;
+    }
+    return answer;
+}
